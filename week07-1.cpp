81e61e9a51b146187580f7f22d0e115b4324a6fb
jeff48872 <112918697+jeff48872@users.noreply.github.com>
2022-10-18 11:28:49 +0800

    week07

---
+++ week07-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int n=9876543210;///可裝9-10位數
+    printf("int 印出來 %d\n",n);///錯的
+
+    long long int a=9876543210;///可裝19-20位數
+    printf("long long int 印出來 %lld\n",a);
+}
