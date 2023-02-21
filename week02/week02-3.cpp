36e1a21664e998e5cb8bf8fda62ea36f396e3fd8
jeff48872 <h24053135@gmail.com>
2023-02-21 11:51:43 +0800

    week02

---
+++ week02/week02-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+	long long int a,b;
+
+	while(std::cin >> a >> b){
+		long long int ans= b - a;
+		if ( ans < 0) ans= a - b;
+		std::cout << ans << std::endl;
+		//printf("%lld\n",ans);
+	}
+}
